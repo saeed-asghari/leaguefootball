bd780adc384895a7669f4d2791445a3f657cfb0c
Saeed Asghari <saeed.asghari.241@gmail.com>
2020-12-22 12:25:15 +0330

    add profile files

---
+++ Profiles/LeaguesProfile.cs
@@ -0,0 +1,21 @@
+using AutoMapper;
+using LeagueFootball.Entities;
+using LeagueFootball.Dtos;
+using LeagueFootball.RequestModels;
+namespace LeagueFootball.Profiles
+{
+    public class LeaguesProfile :Profile
+    {
+        public LeaguesProfile()
+        {
+            //Source -> Target
+            CreateMap<League, LeagueDTO>();
+            CreateMap<Team, TeamDTO>();
+            CreateMap<Player, PlayerDTO>();
+
+            CreateMap<TeamRequestModel,Team>();
+            CreateMap<TeamUpdateRequestModel,Team>();
+
+        }
+    }
+}
