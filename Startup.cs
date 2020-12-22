c7a292c7b0ea83e47dadba1e4bd5286d540afd3f
Saeed Asghari <saeed.asghari.241@gmail.com>
2020-12-22 12:46:50 +0330

    add pre files

--- Startup.cs
+++ Startup.cs
@@ -1,22 +1,16 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System.Text;
-using Newtonsoft.Json;
-using AutoMapper;
 using LeagueFootball.Helpers;
 using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using Newtonsoft.Json.Serialization;
 using LeagueFootball.Interfaces;
+using System.Text;
+using Newtonsoft.Json;
 namespace LeagueFootball
 {
     public class Startup
@@ -43,11 +37,10 @@ namespace LeagueFootball
              services.AddScoped<ILeagueService,SqlLeagueRepo>();
              services.AddScoped<ITeamService,SqlTeamRepo>();
              services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "leaguefootball", Version = "v1" });
-            });
+            // services.AddSwaggerGen(c =>
+            // {
+            //     c.SwaggerDoc("v1", new OpenApiInfo { Title = "leaguefootball", Version = "v1" });
+            // });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,8 +49,8 @@ namespace LeagueFootball
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "leaguefootball v1"));
+              //  app.UseSwagger();
+              //  app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "leaguefootball v1"));
             }
 
             app.UseHttpsRedirection();
